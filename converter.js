281446ef7841653c9bc1cfac8b2cee6da1b1aaf8
shmumboy2 <111930821+shmumboy2@users.noreply.github.com>
2022-09-01 16:00:21 -0700

    got rid of names
    replaced the names with decimal number recorder

--- converter.js
+++ converter.js
@@ -1,10 +1,10 @@
-let num = prompt("What number do you want to be converted")
+let num = document.getElementById("decimal")
 let bin = 0 
 let rem, i = 1, step = 1
-while (num !=0){
+while (num !=0){    
     rem = num % 2
     num = parseInt(num / 2)
         bin = bin + rem * i
         i = i * 10
-} /*Divides number by 2 until = to 0*/
+} /*Divides number by 2 until = to 0 and creates binary number*/
 console.log(bin)
