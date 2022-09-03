15d1ef3c1029b6bba3fe4f39f6c2effd3502f98c
alangd707 <111930791+alangd707@users.noreply.github.com>
2022-09-02 21:42:44 -0700

    Hex Converter functional
     - Added the code that only needs to be called by a button/text box within the HTML.

--- converter.js
+++ converter.js
@@ -10,3 +10,34 @@ while (num !=0){
 } /*Divides number by 2 until = to 0 and creates binary number*/
 }
 
+hexConvert(10000)
+
+function hexConvert(value) {
+    let final = []
+    while (value != 0) {
+        if (value % 16 < 10) {
+            final.push(value % 16)
+        }
+        else if (value % 16 == 10) {
+            final.push('A')
+        }
+        else if (value % 16 == 11) {
+            final.push('B')
+        }
+        else if (value % 16 == 12) {
+            final.push('C')
+        }
+        else if (value % 16 == 13) {
+            final.push('D')
+        }
+        else if (value % 16 == 14) {
+            final.push('E')
+        }
+        else if (value % 16 == 15) {
+            final.push('F')
+        }
+        value = (value - value % 16) / 16
+    }
+    final = final.reverse()
+    console.log(final.join(''))
+}
